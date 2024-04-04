161353f52bf6b9583f5268cbb2665bd352677833
songkyoungmin <165114116+songkyoungmin@users.noreply.github.com>
2024-04-04 16:07:42 +0900

    Title: 물의 상태, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240404/물의 상태/state-of-water.py
@@ -0,0 +1,23 @@
+n = input()
+n = int(n)
+# if n<0:
+#     print("ice")
+# elif n>=100:
+#     print("vapor")
+# else:
+#     print("water") 
+
+# if n>=100:
+#     print("vapor")
+# elif n>0:
+#     print("water")
+# else:
+#     print("ice")
+
+if n>0:
+    if n>=100:
+        print("vapor")
+    else:
+        print("water")
+else:
+    print("ice")
