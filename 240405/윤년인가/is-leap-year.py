675a901ea44bd2ba73391dc28402c0523e445de0
songkyoungmin <165114116+songkyoungmin@users.noreply.github.com>
2024-04-05 15:04:33 +0900

    Title: 윤년인가, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240405/윤년인가/is-leap-year.py
+++ 240405/윤년인가/is-leap-year.py
@@ -1,8 +1,10 @@
 y = input()
 y = int(y)
-if y%4==0 or y%400!=0:
+if y%4==0 and y%400==0:
     print("true")
-elif y%100==0 and y%400!=0:
+elif y%100!=0:
         print("true")
 else:
     print("false")
+
+# 4로 나누어지거나 400으로도 나누어 떨어져야함
