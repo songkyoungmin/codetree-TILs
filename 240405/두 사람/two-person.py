c02c829563caab3454dd7afa8b2eee0bbeaedb14
songkyoungmin <165114116+songkyoungmin@users.noreply.github.com>
2024-04-05 14:07:11 +0900

    Title: 두 사람, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240405/두 사람/two-person.py
@@ -0,0 +1,21 @@
+# age1,gender1 = input().split(" ")
+# age1 = int(age1)
+# age2,gender2 = input().split(" ")
+# age2 = int(age2)
+# if gender1 == M or gender2 == M:
+#     if age1 >=19 or age2 >=19:
+#         print(1)
+# else:
+#     print(0)
+
+a,b = input().split(" ")
+c,d = input().split(" ")
+a = int(a)
+b = str(b)
+c = int(c)
+d = str(d)
+if a>=19 or c>=19:
+    if b== 'M' or d== 'M':
+        print(1)
+else:
+    print(0)
