61b637247bb6d461ef011bcfa4253d6e3efe1d6b
songkyoungmin <165114116+songkyoungmin@users.noreply.github.com>
2024-04-05 12:59:53 +0900

    Title: 특정 조건 두 정수 비교, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240405/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
+++ 240405/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -1,7 +1,15 @@
 a,b=input().split(" ")
 a = int(a)
 b = int(b)
+# if a<b:
+#     print(1,0)  
+# elif a==b:
+#     print(0,1)
 if a<b:
-    print(1,0)  
-elif a==b:
-    print(0,1)
+    print("1",end = " ")
+else:
+    print("0",end = " ")
+if a == b:
+    print("1")
+else:
+    print("0")
