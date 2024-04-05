e76c37c84831df8e5e0d10d920134b84d61fbc00
songkyoungmin <165114116+songkyoungmin@users.noreply.github.com>
2024-04-05 13:51:21 +0900

    Title: 숫자의 계절은, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240405/숫자의 계절은/season-of-num.py
@@ -0,0 +1,10 @@
+m = input()
+m = int(m)
+if m==3 or m==4 or m==5:
+    print("Spring")
+if m==6 or m==7 or m==8:
+    print("Summer")
+if m==9 or m==10 or m==11:
+    print("Fall")
+if m==12 or m==1 or m==2:
+    print("Winter")
