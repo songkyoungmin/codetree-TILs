663892d284e84983d60e073ad190641676224ccd
songkyoungmin <165114116+songkyoungmin@users.noreply.github.com>
2024-04-05 15:18:15 +0900

    Title: 윤년인가, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240405/윤년인가/is-leap-year.py
+++ 240405/윤년인가/is-leap-year.py
@@ -20,9 +20,20 @@
 # else:
 #     print("flase")
 
+# y = input()
+# y = int(y)
+# if y%100!=0 or (y%400==0 and y%4==0):
+#     print("true")
+# else:
+#     print("false")
+
 y = input()
 y = int(y)
-if y%100!=0 or (y%400==0 and y%4==0):
+if y%400==0:
+    print("true")
+elif y%100==0:
+    print("false")
+elif y%4==0:
     print("true")
 else:
     print("false")
