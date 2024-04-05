aea84794c83ba1555eaaa81eaf70fe650d2136ad
songkyoungmin <165114116+songkyoungmin@users.noreply.github.com>
2024-04-05 13:33:29 +0900

    Title: 특정 조건 세 정수 비교, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240405/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,12 @@
+a,b,c = input().split(" ")
+a = int(a)
+b = int(b)
+c = int(c)
+if a<=b and a<=c:
+    print(1,end = " ")
+else:
+    print(0,end = " ")
+if a==b==c:
+    print(1)
+else:
+    print(0)
