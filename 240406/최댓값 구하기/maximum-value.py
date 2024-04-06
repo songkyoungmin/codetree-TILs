8c0d8665e26a750630bf9622e151ebe39a73deb7
songkyoungmin <165114116+songkyoungmin@users.noreply.github.com>
2024-04-06 13:50:49 +0900

    Title: 최댓값 구하기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240406/최댓값 구하기/maximum-value.py
+++ 240406/최댓값 구하기/maximum-value.py
@@ -2,11 +2,9 @@ a,b,c = input().split(" ")
 a = int(a)
 b = int(b)
 c = int(c)
-if a>=b and a>=c:
+if a>=b>=c and a>=c>=b:
     print(a)
-elif b>a and b>=c:
+elif b>=a>=c and b>=c>=a:
     print(b)
-elif c>a and c>b:
-    print(c)
 else:
-    print(" ")
+    print(c)
