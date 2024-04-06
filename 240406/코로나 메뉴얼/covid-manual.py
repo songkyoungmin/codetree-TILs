ab6d1338a6304b141fcd84bdc749f4f19d61e9e7
songkyoungmin <165114116+songkyoungmin@users.noreply.github.com>
2024-04-06 13:37:38 +0900

    Title: 코로나 메뉴얼, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240406/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,68 @@
+# # res1,tem1 = input().split(" ")
+# # res2,tem2 = input().split(" ")
+# # res3,tem3 = input().split(" ")
+# # res1 = str(res1)
+# # res2 = str(res2)
+# # res3 = str(res3)
+# # tem1 = int(tem1)
+# # tem2 = int(tem2)
+# # tem3 = int(tem3)
+# # if res1=='Y' and tem1>=37:
+# #     print("A")
+# # elif res1 == 'N' and tem1>=37:
+# #     print("B")
+# # elif res1 == 'Y'and tem1<=37:
+# #     print("C")
+# # else:
+# #     print("D")
+# # if res2=='Y' and tem2>=37:
+# #     print("A")
+# # elif res2 == 'N' and tem2>=37:
+# #     print("B")
+# # elif res2 == 'Y'and tem2<=37:
+# #     print("C")
+# # else:
+# #     print("D")
+# # if res3=='Y' and tem3>=37:
+# #     print("A")
+# # elif res3 == 'N' and tem3>=37:
+# #     print("B")
+# # elif res3 == 'Y'and tem3<=37:
+# #     print("C")
+# # else:
+# #     print("D")
+# # if 'A'>=2:
+# #     print("E")
+# # else:
+# #     print("N")\
+
+# res1,tem1 = input().split(" ")
+# res2,tem2 = input().split(" ")
+# res3,tem3 = input().split(" ")
+# res1 = str(res1)
+# res2 = str(res2)
+# res3 = str(res3)
+# tem1 = int(tem1)
+# tem2 = int(tem2)
+# tem3 = int(tem3)
+# 'A'= res1=='Y'and tem1>=37
+# 'B'= res2=='N'and tem2>=37
+# 'C'= res3 =='Y'and tem3<37
+# 'D'= 
+
+res1,tem1 = input().split(" ")
+res2,tem2 = input().split(" ")
+res3,tem3 = input().split(" ")
+res1 = str(res1)
+res2 = str(res2)
+res3 = str(res3)
+tem1 = int(tem1)
+tem2 = int(tem2)
+tem3 = int(tem3)
+if res1 == 'Y' and tem1>=37:
+    if res2 == 'Y' and tem2>=37 or (res3 == 'Y' and tem3>=37):
+        print('E')
+    else:
+        print('N')
+else:
+    print('N')
