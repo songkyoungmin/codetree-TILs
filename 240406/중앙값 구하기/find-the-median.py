46b3e992824dbb4a6d795951a6cc9f219ea22935
songkyoungmin <165114116+songkyoungmin@users.noreply.github.com>
2024-04-06 13:49:14 +0900

    Title: 중앙값 구하기, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240406/중앙값 구하기/find-the-median.py
@@ -0,0 +1,18 @@
+a,b,c = input().split(" ")
+a = int(a)
+b = int(b)
+c = int(c)
+# if a>=b>=c:
+#     if c>=b>=a:
+#         print(b)
+#     elif a>=c>=b:
+#         if b>=c>=a:
+#             print(c)
+# else:
+#     print(a)
+if (a>=b>=c) or (c>=b>=a):
+    print(b)
+elif (a>=c>=b) or (b>=c>=a):
+    print(c)
+else:
+    print(a)
