873b99a71fefbcbd6f3b13eb7128007d78ede19e
songkyoungmin <165114116+songkyoungmin@users.noreply.github.com>
2024-04-11 13:01:08 +0900

    Title: 정사각형의 넓이, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240411/정사각형의 넓이/area-of-a-rectangle.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+area = n*n
+# if n < 5:
+#     # print(res1,"tiny",sep = "\n")
+#     print(res1)
+#     print("tiny")
+# if n >= 5:
+#     print(area)    
+print(area)
+if n < 5:
+    print("tiny")
