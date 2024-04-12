4a185c8a169a8aecff2a999767c2d94345768c9a
songkyoungmin <165114116+songkyoungmin@users.noreply.github.com>
2024-04-12 14:35:28 +0900

    Title: 윤년인가, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240412/윤년인가/is-leap-year.py
@@ -0,0 +1,49 @@
+# y = input()
+# y = int(y)
+# if y%4==0 or y%400!=0:
+#     print("true")
+# elif y%100!=0:
+#         print("true")
+# else:
+#     print("false")
+
+# 결론은 4로 나누어지거나 400으로도 나누어 떨어져야함
+
+# y = input()
+# y = int(y)
+# if y%400==0:
+#     print("true")
+# elif y%100==0:
+#     print("false")
+# elif y%4==0:
+#     print("true")
+# else:
+#     print("flase")
+
+# y = input()
+# y = int(y)
+# if y%100!=0 or (y%400==0 and y%4==0):
+#     print("true")
+# else:
+#     print("false")
+
+y = input()
+y = int(y)
+# if y%400==0:
+#     print("true")
+# elif y%100==0:
+#     print("false")
+# elif y%4==0:
+#     print("true")
+# else:
+#     print("false")
+if y%4==0:
+    if y%100==0:
+        if y%400!=0:
+            print("flase")
+        else:
+            print("true")
+    else:
+        print("true")
+else:
+    print("false")
