2a455cc7060a4f7a408cd34c8022bf705b215d2d
songkyoungmin <165114116+songkyoungmin@users.noreply.github.com>
2024-05-03 11:40:43 +0900

    Title: 홀수만 출력, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240503/홀수만 출력/output-only-odd.py
@@ -0,0 +1,9 @@
+a,b = input().split(" ")
+a = int(a)
+b = int(b)
+if a % 2==0:
+    for i in range(a+1,b+1,2):
+        print(i, end = " ")
+else:
+    for i in range(a,b+1,2):
+        print(i,end = " ")
